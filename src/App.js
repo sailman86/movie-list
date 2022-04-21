fa73512ffb30585c0cf375a6868d696ef303f29b
Claudio Famiglietti <claudio.famiglietti@gmail.com>
2022-04-21 18:17:26 +0200

    Api OMDb - initial Css Style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import './search.svg';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=246bb98b'
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    searchMovies('batman')
+  }, []);
+
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <h1>MovieLand</h1>
+    </div>
   );
 }
 
